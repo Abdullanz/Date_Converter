0ca9f447e778be027dcdc86402be09d96e9cd66d
Abdullanz <amnjrx@gmail.com>
2020-03-30 19:20:25 +0300

    Progress ;')

--- Main.py
+++ Main.py
@@ -1,40 +1,40 @@
-import PySimpleGUI as sg
-from hijri_converter import convert
-
 #################################################################################
 # Description: A date conversion system that converts Islamic date to Georgion
 # date with a simple user interface.
 # Bugs count: 0
-# Fixes needed:
+# TODO: I need to convert the input string into seperate entitites day, month, and year then insert these values inside the conversion API. Make sure IN/OP are linked
 #################################################################################
+import PySimpleGUI as sg #GUI library
+from hijri_converter import convert #conversion library
+import re #regex library
 
-#GUI
-sg.theme('DarkAmber')	# Add a touch of color
+# GUI - Added a dark theme
+sg.theme('DarkAmber')
 
-# All the stuff inside your window.
-layout = [  [sg.Text('Convert from Arabic Calender to Georg Calender')],
-            [sg.Text('Enter Arabic date in format (YYYY/MM/DD):'), sg.InputText()],
-            [sg.Text('Enter Georgion date in format (YYYY/MM/DD):'), sg.InputText()],
-        #    [sg.OutputText()],
-        #    [sg.OutputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+# Layout of elements
+layout = [  [sg.Text('Convert from Arabic Calender to Georg Calender: ')],
+            [sg.Text('Enter Arabic date in format (DD/MM/YYYY):'), sg.InputText()],
+            # [sg.Text('Enter Georg. date in format (DD/MM/YYYY):'), sg.InputText()],
+            [sg.Output()],
+            [sg.Button('Ok'), sg.Button('Cancel')]]
 
-# Create the Window
+#Regex for date: ^\d{1,2}\/\d{1,2}\/\d{4}$
+
+# Window
 window = sg.Window('Date Converter', layout)
-# Event Loop to process "events" and get the "values" of the inputs
+
+# Event Loop to process the inputs
 while True:
     event, values = window.read()
-    if event in(None, 'Cancel'):	# if user closes window or clicks cancel
+    if event in(None, 'Cancel'):
         break
-    print('You entered ', values[0])
+    if event == 'Ok':
+        g = convert.Hijri(1403, 2, 17).to_gregorian()
+    print('The result in Georg. Calender: ', values[0])
+    # if event == 'Ok':
+    #     h = convert.Gregorian(1982, 12, 2).to_hijri()
+    # print('The result in Arab Calender: ', values[g])
+# g - 1982-12-02
+# h - 1403-02-17
 
 window.close()
-
-
-#g = convert.Hijri(1403, 2, 17).to_gregorian()
-#print(g)
-# 1982-12-02
-
-#h = convert.Gregorian(1982, 12, 2).to_hijri()
-#print(h)
-# 1403-02-17
